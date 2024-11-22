38f20bb4c9f1a6aeb3c6382fe00780aa5f1d9a0a
Eij,Emiel E.M.H <i373939@fhict.nl>
2024-11-22 15:01:43 +0100

    finished the generating of code for TickTackToe

---
+++ Klara/no.ntnu.tdt4250.Games/GeneratedTickTackToe/TickTackToeRules.java
@@ -0,0 +1,66 @@
+class TickTackToeRules
+{
+	// Attribute declaration	
+	private int rows = 3;
+	private int columns = 3;
+	
+    public boolean checkWinner(char[][] board, char symbol) {
+      for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns - 3; j++) {
+        if (board[i][j] == symbol
+            && board[i][j + 1] == symbol
+            && board[i][j + 2] == symbol
+            && board[i][j + 3] == symbol) {
+          return true;
+        }
+      }
+    }
+
+    // Check vertical
+    for (int i = 0; i < rows - 3; i++) {
+      for (int j = 0; j < columns; j++) {
+        if (board[i][j] == symbol
+            && board[i + 1][j] == symbol
+            && board[i + 2][j] == symbol
+            && board[i + 3][j] == symbol) {
+          return true;
+        }
+      }
+    }
+
+    // Check diagonal (top-left to bottom-right)
+    for (int i = 0; i < rows - 3; i++) {
+      for (int j = 0; j < columns - 3; j++) {
+        if (board[i][j] == symbol
+            && board[i + 1][j + 1] == symbol
+            && board[i + 2][j + 2] == symbol
+            && board[i + 3][j + 3] == symbol) {
+          return true;
+        }
+      }
+    }
+
+    // Check diagonal (bottom-left to top-right)
+    for (int i = 3; i < rows; i++) {
+      for (int j = 0; j < columns - 3; j++) {
+        if (board[i][j] == symbol
+            && board[i - 1][j + 1] == symbol
+            && board[i - 2][j + 2] == symbol
+            && board[i - 3][j + 3] == symbol) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+    
+	public boolean isBoardFull(char[][] board) {
+    for (int j = 0; j < columns; j++) {
+      if (board[0][j] == '.') {
+        return false;
+      }
+    }
+    return true;
+  }
+}
